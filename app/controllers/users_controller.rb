3d80f92652d646f630c3f3d686542ca84299b9ee
Weston Mizumoto <westonmizumoto@Westons-MacBook-Pro-2.local>
2014-11-22 00:55:47 -0800

    now shows best time in profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,12 @@ class UsersController < ApplicationController
 		}
 	end	
 	def show()
+		@PBs = Hash.new
 		@user = User.find(params[:id])
+		allEventArray = ["2x2","3x3","4x4","5x5", "6x6","7x7", "3x3 OH","Square-1", "Megaminx","Pyraminx", "Clock","Skewb"]
+		allEventArray.each do |event|
+			@PBs[event] = getPbForEvent(@user.id,event)
+		end
 	end
 	def login()
 	end
@@ -44,7 +49,25 @@ class UsersController < ApplicationController
 		else
 			render:new	
 		end
-	end
+		
 
+
+
+
+	end
+	def getPbForEvent(userId,eventName)
+		allSubmissions = Submission.where("user_id = ?",  userId.to_s)
+		bestTime = nil
+		allSubmissions.each do |sub|
+			if(sub.event.event_name == eventName && (bestTime == nil || sub.result < bestTime)) then
+				bestTime = sub.result
+			end
+		end	
+		if(bestTime != nil) then
+			return bestTime.to_s
+		end
+		return "-"
+	end
+	
 	
 end
